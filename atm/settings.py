d4ddfb7c96efa3c6388e4f12c02dbbfbdb8a5ab0
ullyzian <aviev2000@icloud.com>
2020-05-17 18:58:14 +0200

    Last production preparations

--- atm/settings.py
+++ atm/settings.py
@@ -17,9 +17,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://atm-bankomat.herokuapp.com"]
 
 
 # Application definition
